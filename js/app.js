ba7f406c0d0cfd12d7b6899b64cd638c1726ad1b
Daniel151505 <daniel150520011@gmail.com>
2021-01-05 13:01:04 -0500

    Creando Variables

---
+++ js/app.js
@@ -0,0 +1,15 @@
+//Variables
+
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarrito = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+let articulosCarrito = [];
+
+
+    
+
+
+
+
+
