b004decb841d13f083178b9f8e94f8e5612f0a33
Daniel151505 <daniel150520011@gmail.com>
2021-01-05 13:06:29 -0500

    Creando función leerDatosCurso

--- js/app.js
+++ js/app.js
@@ -15,9 +15,36 @@ cargarEventListeners();
     }
 
 
+    //funciones
 
+    function agregarCurso(e){
+        e.preventDefault();
+        if(e.target.classList.contains('agregar-carrito')){
+            const cursoSeleccionado = e.target.parentElement.parentElement;
+            leerDatosCurso(cursoSeleccionado);
+
+        }
+    }
     
+    // Lee el contenido del Html al dar click y extrae la información
 
+    function leerDatosCurso(curso){
+
+        //Creando obeto con el contenido del Curso Seleccionado
+
+        const infoCurso = {
+            imagen: curso.querySelector('img').src,
+            titulo: curso.querySelector('h4').textContent,
+            precio: curso.querySelector('.precio span').textContent,
+            id:curso.querySelector('a').getAttribute('data-id'),
+            cantidad: 1
+        }
+
+        //Agrega elementos al arreglo de carrito
+
+        articulosCarrito = [...articulosCarrito,infoCurso];
+
+    }
 
 
 
