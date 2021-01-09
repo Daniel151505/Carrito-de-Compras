04f3491a684acb4c9a28417cff94b54b48601c7f
Daniel151505 <daniel150520011@gmail.com>
2021-01-08 21:51:11 -0500

    Agregando valores para mostrar en el carrito

--- js/app.js
+++ js/app.js
@@ -59,12 +59,14 @@ cargarEventListeners();
 
         //Recorre el carrito y genera el HTML
         articulosCarrito.forEach( curso =>{
-
+            const {imagen,titulo,precio,cantidad,id} = curso
             const row = document.createElement('tr');
             row.innerHTML = `
-            <td>
-                  ${curso.titulo}         
-            </td> 
+            <td><img src="${imagen}" width="100"></td> 
+            <td>${titulo}</td> 
+            <td> ${precio}</td>
+            <td> ${cantidad}</td>
+            <td><a href="#" class="borrar-curso" data-id="${id}"> x </td>
             `;
             
 
